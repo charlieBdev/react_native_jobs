ac71c57de5a313cd525f27adcc6fc0021e90d48b
bigspambot <67501435+bigspambot@users.noreply.github.com>
2024-04-05 16:55:50 +0100

    Welcome component layout done

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,24 @@
+import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
+import { useCallback } from 'react';
+import * as Splashscreen from 'expo-splash-screen';
+
+Splashscreen.preventAutoHideAsync();
 
 const Layout = () => {
-	return <Stack />;
+	const [fontsLoaded] = useFonts({
+		DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+		DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+		DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+	});
+
+	const onLayoutRouteView = useCallback(async () => {
+		if (fontsLoaded) await Splashscreen.hideAsync();
+	}, [fontsLoaded]);
+
+	if (!fontsLoaded) return null;
+
+	return <Stack onLayout={onLayoutRouteView} />;
 };
 
 export default Layout;
